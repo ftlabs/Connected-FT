4992e615d386e0d4dc7e398cae713775290243eb
Sean Tracey <seanmtracey@gmail.com>
2017-04-24 23:35:57 +0100

    try-catch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const envVars = {};
-require('fs').readFileSync('.env', 'utf8').split('\n').forEach(KV => {
 
-	const key = KV.split('=')[0];
-	const value = KV.split('=')[1];
+try{
 
-	envVars[key] = value;
+	require('fs').readFileSync('.env', 'utf8').split('\n').forEach(KV => {
 
-});
+		const key = KV.split('=')[0];
+		const value = KV.split('=')[1];
+
+		envVars[key] = value;
+
+	});
+
+} catch(err){
+	console.log(err);
+}
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
