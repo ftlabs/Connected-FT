6d84c1b9d81e38a4bb2911f068431ceffb11d2fb
Sean Tracey <seanmtracey@gmail.com>
2017-06-01 18:09:46 +0100

    Trigger device by type.

--- routes/notifications.js
+++ routes/notifications.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const devices = require('../bin/lib/devices');
+const convertSessionToUserID = require('../bin/lib/convert-session-to-userid');
 
 const webPush = require('web-push');
 webPush.setGCMAPIKey(process.env.GCM_API_KEY);
@@ -16,21 +17,25 @@ webPush.setVapidDetails(
 	vapidKeys.privateKey
 );
 
+function triggerNotification(subscription, payload){
+	webPush.sendNotification(subscription, JSON.stringify(payload) );
+}
+
 router.post('/trigger/:DEVICE_ID', (req, res) => {
 
 	const data = req.body;
-	const device = req.params['DEVICE_ID']
+	const device = req.params['DEVICE_ID'];
 
 	debug('TRIGGER', data, device);
 
 	devices.get(device)
 		.then(deviceDetails => {
+			triggerNotification(deviceDetails.subscription, data);
 			debug(deviceDetails);
-			webPush.sendNotification(deviceDetails.subscription, JSON.stringify(data) );
 			res.json({
 				status : 'ok',
 				message : `Notification triggered for ${device}`
-			})
+			});
 		})
 		.catch(err => {
 			debug(err);
@@ -44,6 +49,36 @@ router.post('/trigger/:DEVICE_ID', (req, res) => {
 
 });
 
+router.get('/trigger/type/:DEVICE_TYPE(phone|tablet|computer)', convertSessionToUserID, (req, res) => {
 
+	const data = req.body;
+	const requestedType = req.params['DEVICE_TYPE'];
+
+	devices.list(res.locals.userid)
+		.then( devices => devices.filter( device => { return device.type === requestedType; } ) )
+		.then( deviceOfType => {
+
+			deviceOfType = deviceOfType[0];
+
+			if(deviceOfType === undefined){
+				res.status(404);
+				res.json({
+					status : 'err',
+					message : 'A device of this type was not found for this user'
+				});	
+			} else {
+				triggerNotification(deviceOfType.subscription, data);
+				res.json({
+					status : 'ok',
+					message : `Notification triggered for ${requestedType}`
+				});
+			}
+
+		} )
+	;
+
+	res.end();
+
+});
 
 module.exports = router;
